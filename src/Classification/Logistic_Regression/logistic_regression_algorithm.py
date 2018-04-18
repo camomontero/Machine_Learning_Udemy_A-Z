aa7933f0628c47d89f88d949e0132a664db37c62
camomontero <camomonteroed@gmail.com>
2018-04-18 12:02:58 +0200

    Adding the confusion matrix code for the Logistic Regression classifier

--- src/Classification/Logistic_Regression/logistic_regression_algorithm.py
+++ src/Classification/Logistic_Regression/logistic_regression_algorithm.py
@@ -10,6 +10,7 @@ Created on Wed Apr 11 16:19:06 2018
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+import seaborn
 
 # Importing the dataset
 dataset=pd.read_csv('Social_Network_Ads.csv')
@@ -58,7 +59,7 @@ plt.ylim(X2.min(), X2.max())
 for i, j in enumerate(np.unique(y_set)):
     plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
                 c = ListedColormap(('red', 'green'))(i), label = j)
-plt.title('Classifier (Training set)')
+plt.title('Logistic Regression Classifier (Training set)')
 plt.xlabel('Age')
 plt.ylabel('Estimated Salary')
 plt.legend()
@@ -76,8 +77,12 @@ plt.ylim(X2.min(), X2.max())
 for i, j in enumerate(np.unique(y_set)):
     plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
                 c = ListedColormap(('red', 'green'))(i), label = j)
-plt.title('Classifier (Test set)')
+plt.title('Logistic Regression Classifier (Test set)')
 plt.xlabel('Age')
 plt.ylabel('Estimated Salary')
 plt.legend()
 plt.show()
+
+# Visualizing the Confusion Matrix
+df_cm = pd.DataFrame(cm, index= ['R E D', 'G R E E N'])
+seaborn.heatmap(df_cm, annot=True).set_title("Confusion Matrix (Logistic Regression Classifier)")
